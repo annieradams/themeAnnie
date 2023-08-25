4229b8fe690fefe5aa4f3c3cdba4f4c4064983f7
annieradams <adams.annie11@gmail.com>
2023-08-25 11:01:42 -0700

    updated colors

--- R/theme_outdoors.R
+++ R/theme_outdoors.R
@@ -5,8 +5,8 @@
 #'
 #' @examples
 theme_outdoors <-  function( ){
-  theme(plot.background = element_rect(fill = "brown"),
-        panel.background = element_rect(fill = "green"),
+  theme(plot.background = element_rect(fill = "#964B00"),
+        panel.background = element_rect(fill = "#78866b"),
         axis.text = element_text(color = "yellow"),
-        panel.grid = element_line(color = "blue"))
+        panel.grid = element_line(color = "#CC5500"))
 }
